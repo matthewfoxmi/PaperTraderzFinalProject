aec21959903d06f962ca362a9d3b09f42dfea189
Josh <thejosh2021@gmailcom>
2022-08-16 14:44:15 -0400

    inital commit

---
+++ Updatingto14/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,35 @@
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent implements OnInit {
+  isExpanded = false;
+
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
+
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+}
