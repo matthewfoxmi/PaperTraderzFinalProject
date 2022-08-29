1c25630d982784f4def4d378ae344699111d6201
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-08-29 09:46:01 -0400

    adjusted StonksDBContext to guard classified info

--- Updatingto14/Models/StonksDBContext.cs
+++ Updatingto14/Models/StonksDBContext.cs
@@ -26,7 +26,7 @@ namespace Updatingto14.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=papertraderz.database.windows.net;Initial Catalog=StonksDB; User Id=TeamChungusStonks; Password=F4ncylivingherewecome;");
+                optionsBuilder.UseSqlServer($"Data Source={Secret.server};Initial Catalog=StonksDB; User Id={Secret.userName}; Password={Secret.password};");
             }
         }
 
