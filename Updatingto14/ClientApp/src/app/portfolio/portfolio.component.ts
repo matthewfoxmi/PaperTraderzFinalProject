528394c1ebac676b409d955f6536719992dba545
Connor <Woodconnor13@gmail.com>
2022-09-01 10:12:08 -0400

    added basic styling to portfolio and stock list, sign in on portfolio page now works without refresh

--- Updatingto14/ClientApp/src/app/portfolio/portfolio.component.ts
+++ Updatingto14/ClientApp/src/app/portfolio/portfolio.component.ts
@@ -32,49 +32,49 @@ export class PortfolioComponent implements OnInit {
       this.user = user;
       UserService.user.id = user.id;
       this.loggedIn = (user != null);
+      //calls to SQL db and returns the invested stocks as an array
+      this.investedStockService.getAllInvested().subscribe((response:InvestedStock[]) => {
+        this.allStonksOwnedByUser = response;
+        //sorts all tickers in array alphabetically
+        this.allStonksOwnedByUser.sort((a, b) => a.investedTicker.localeCompare(b.investedTicker))
+        //console.log(response)
+        //3 lines below grabs the tickers and combines into one string to pass into api for calling those stocks
+        let tickers:string = "";
+        this.allStonksOwnedByUser.forEach((s:any) => {
+          tickers += s.investedTicker+",";
+          //console.log(tickers)        
+        });
+        //THIS WHOLE CHUNK CALCULATES PORTFOLIO VALUE
+        //ticker string from above is used to call api and return the stock data ordered alphabetically by ticker
+        this.stonkService.getApiStonks(tickers).subscribe((response:any) => {
+          this.stonk = response;
+          if(response.count < 1000){
+            let index = 0;
+            this.stonk.tickers.sort((a, b) => a.ticker.localeCompare(b.ticker))
+            this.allStonksOwnedByUser.forEach((s:any) => {
+              this.portfolioValue += (s.sharesOwned * this.stonk.tickers[index].day.c);
+              index += 1;
+            })
+            this.userService.getUserById(this.user.id).subscribe((response:any) => {
+              this.currentCash = response.currentCash;
+              console.log(response.currentCash);
+              this.portfolioValue += this.currentCash;
+              this.portfolioValue = Number(this.portfolioValue.toFixed(2));
+            });
+            console.log(this.portfolioValue);
+            //console.log(response)
+            this.isEmpty = true;
+          }else{
+            this.isEmpty = false;
+            this.userService.getUserById(this.user.id).subscribe((response:any) => {
+              this.currentCash = response.currentCash;
+              console.log(response.currentCash);
+              this.portfolioValue += this.currentCash;
+              this.portfolioValue = Number(this.portfolioValue.toFixed(2));
+            });
+          }
+        });
+      });
     })
-    //calls to SQL db and returns the invested stocks as an array
-    this.investedStockService.getAllInvested().subscribe((response:InvestedStock[]) => {
-      this.allStonksOwnedByUser = response;
-      //sorts all tickers in array alphabetically
-      this.allStonksOwnedByUser.sort((a, b) => a.investedTicker.localeCompare(b.investedTicker))
-      //console.log(response)
-      //3 lines below grabs the tickers and combines into one string to pass into api for calling those stocks
-      let tickers:string = "";
-      this.allStonksOwnedByUser.forEach((s:any) => {
-        tickers += s.investedTicker+",";
-        //console.log(tickers)        
-      });
-      //THIS WHOLE CHUNK CALCULATES PORTFOLIO VALUE
-      //ticker string from above is used to call api and return the stock data ordered alphabetically by ticker
-      this.stonkService.getApiStonks(tickers).subscribe((response:any) => {
-        this.stonk = response;
-        if(response.count < 1000){
-          let index = 0;
-          this.stonk.tickers.sort((a, b) => a.ticker.localeCompare(b.ticker))
-          this.allStonksOwnedByUser.forEach((s:any) => {
-            this.portfolioValue += (s.sharesOwned * this.stonk.tickers[index].day.c);
-            index += 1;
-          })
-          this.userService.getUserById(this.user.id).subscribe((response:any) => {
-            this.currentCash = response.currentCash;
-            console.log(response.currentCash);
-            this.portfolioValue += this.currentCash;
-            this.portfolioValue = Number(this.portfolioValue.toFixed(2));
-          });
-          console.log(this.portfolioValue);
-          //console.log(response)
-          this.isEmpty = true;
-        }else{
-          this.isEmpty = false;
-          this.userService.getUserById(this.user.id).subscribe((response:any) => {
-            this.currentCash = response.currentCash;
-            console.log(response.currentCash);
-            this.portfolioValue += this.currentCash;
-            this.portfolioValue = Number(this.portfolioValue.toFixed(2));
-          });
-        }
-      });
-    });
   }
 }
